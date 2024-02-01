480a88583a64299fc24e9d32297c175914523f41
aymen.bouhaha <aymen.bouhaha@carnet.tn>
2024-02-01 21:40:06 +0100

    add logic to shop component

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {Observable, tap} from "rxjs";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable, Subscription, tap} from "rxjs";
 import {Product} from "../core/models/base-models/product/product";
 import {Store} from "@ngrx/store";
 import {getProducts, getProductsError, ProductState} from "./store/product.reducer";
@@ -11,29 +11,35 @@ import {ActivatedRoute, Router} from "@angular/router";
   templateUrl: './shop.component.html',
   styleUrls: ['./shop.component.css']
 })
-export class ShopComponent implements OnInit{
+export class ShopComponent implements OnInit , OnDestroy{
 
   products$ : Observable<Product[]>
   error$ : Observable<string | null>
+  subscriptions : Subscription[]=[]
 
   constructor(private store : Store<ProductState>,private activatedRoute : ActivatedRoute) {
-    this.activatedRoute.queryParams.pipe(
+    const queryParams$=this.activatedRoute.queryParams.pipe(
       tap((params)=>{
-
         console.log(params)
         this.store.dispatch(ProductsActions.startFetchingProducts({params : {page : +params["page"], category : params["category"]}}))
       })
     ).subscribe()
     this.products$ = this.store.select(getProducts)
     this.error$ = this.store.select(getProductsError)
-    this.error$.pipe(
-      tap((value)=>{
-        if (value){
-          console.log(value)
-          this.store.dispatch(ProductsActions.clearProductError())
-        }
-      })
+    const error= this.error$.subscribe(
+      (value)=>{
+        console.log(error)
+      }
     )
+    this.subscriptions.push(queryParams$,error)
+  }
+
+  ngOnDestroy(): void {
+      this.subscriptions.forEach(
+        (el)=>{
+          el.unsubscribe()
+        }
+      )
   }
 
   ngOnInit(): void {
