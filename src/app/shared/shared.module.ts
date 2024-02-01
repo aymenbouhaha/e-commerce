b32dc64bd5b4b1e034c1f232326da65a287d6536
Ahmed Frikha <ahmed.frikha@carnet.tn>
2024-02-02 00:52:03 +0100

    sign in ui

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -12,6 +12,9 @@ import { MatCardModule } from '@angular/material/card';
 import { ItemComponent } from './components/item/item.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { ServicesComponent } from './components/services/services.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
@@ -23,7 +26,7 @@ import { ServicesComponent } from './components/services/services.component';
     ItemComponent,
     ServicesComponent,
   ],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatInputModule, MatFormFieldModule],
   exports: [
     FooterComponent,
     HeaderComponent,
@@ -36,6 +39,9 @@ import { ServicesComponent } from './components/services/services.component';
     ItemComponent,
     MatGridListModule,
     ServicesComponent,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
   ],
 })
 export class SharedModule {}
