b4a7657c748c817d2f51df0c95781b1520083839
aymen.bouhaha <aymen.bouhaha@carnet.tn>
2024-02-05 06:51:32 +0100

    changes

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import {map, Observable, tap} from "rxjs";
 import {BasketState, getBasketError, getBasketLoading, getBasketProducts} from "./store/cart.reducer";
 import {Store} from "@ngrx/store";
-import {removeFromBasketStart} from "./store/cart.actions";
+import {clearError, removeFromBasketStart} from "./store/cart.actions";
 import {Product} from "../core/models/base-models/product/product";
 import {GenericComponent} from "../shared/generic/generic.component";
 import {MatDialog} from "@angular/material/dialog";
@@ -17,22 +17,22 @@ import {selectOrdersLoading} from "../account/orders/Store/orders.reducer";
 export class CartComponent extends GenericComponent{
   currentTab: string = 'Tab1';
 
-  basketProductsToOrder$: { id: number, itemsNumber: number }[];
-  basketProducts: {
+  basketProductsToOrder!: { id: number, itemsNumber: number }[];
+  basketProducts!: {
     product : Product
     itemsNumber : number
   }[]
   loading$ : Observable<boolean>
-  orderloading$ : Observable<boolean>
+  orderLoading$ : Observable<boolean>
 
   constructor(private store: Store< BasketState >,private dialog: MatDialog) {
-    super(store.select(getBasketError),store,dialog)
+    super(store.select(getBasketError),store,dialog,clearError)
     this.loading$=this.store.select(getBasketLoading);
-    this.orderloading$=this.store.select(selectOrdersLoading);
+    this.orderLoading$=this.store.select(selectOrdersLoading);
     this.store.select(getBasketProducts).subscribe(
       (value => {
           this.basketProducts=value
-        this.basketProductsToOrder$=this.basketProducts.map(productItem => ({
+        this.basketProductsToOrder=this.basketProducts.map(productItem => ({
           id: productItem.product.id!,
           itemsNumber: productItem.itemsNumber
         }))
@@ -49,7 +49,6 @@ export class CartComponent extends GenericComponent{
   }
 
   addOrder(){
-
     // this.store.dispatch(createOrder({products :this.basketProductsToOrder$ }));
   }
 }
