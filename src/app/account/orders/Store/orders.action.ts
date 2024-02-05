147959b03d979974e4d5fbe19c083fcb4c3eaa98
Ahmed Frikha <ahmed.frikha@carnet.tn>
2024-02-05 10:44:56 +0100

    clear state basket & order

--- src/app/account/orders/Store/orders.action.ts
+++ src/app/account/orders/Store/orders.action.ts
@@ -1,20 +1,19 @@
 import { createAction, props } from '@ngrx/store';
 import { Order } from 'src/app/core/models/base-models/order/order';
-import {Product} from "../../../core/models/base-models/product/product";
+import { Product } from '../../../core/models/base-models/product/product';
 
 export const START_FETCHING_ORDERS = '[ORDERS] START_FETCHING_ORDERS';
 export const FETCHED_ORDERS = '[ORDERS] FETCHED_ORDERS';
 export const ORDERS_ERROR = '[ORDERS] ORDERS ERROR';
 
-export const MAKE_ORDER = '[ORDERS] MAKE_ORDER'
+export const MAKE_ORDER = '[ORDERS] MAKE_ORDER';
 
 export const CLEAR_ORDER_ERROR = '[ORDERS] CLEAR_ORDER_ERROR';
 
-
-export const MAKE_ORDER_SUCCESS = '[ORDERS] MAKE_ORDER_SUCCESS'
+export const MAKE_ORDER_SUCCESS = '[ORDERS] MAKE_ORDER_SUCCESS';
+export const CLEAR_STATE = '[ORDERS] CLEAR_STATE';
 
 export const startFetchingOrders = createAction(START_FETCHING_ORDERS);
-
 
 export const fetchedOrders = createAction(
   FETCHED_ORDERS,
@@ -28,12 +27,13 @@ export const orderError = createAction(
 
 export const clearOrderError = createAction(CLEAR_ORDER_ERROR);
 
-export const makeOrder= createAction(
+export const makeOrder = createAction(
   MAKE_ORDER,
-  props<{products : { product: Product, itemsNumber: number }[]}>()
-)
+  props<{ products: { product: Product; itemsNumber: number }[] }>()
+);
 
-export const makeOrderSuccess= createAction(
+export const makeOrderSuccess = createAction(
   MAKE_ORDER,
-  props<{products : Order}>()
-)
+  props<{ products: Order }>()
+);
+export const clearState = createAction(CLEAR_STATE);
